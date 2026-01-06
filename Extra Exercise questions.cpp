fa8991d51795c238366679069b3320a1576f61a4
f2024332051-dev <f2024332051@umt.edu.pk>
2026-01-06 11:10:57 +0500

    Add files via upload

---
+++ Extra Exercise questions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class student{
+private:
+    string name;
+    int roll;
+public:
+    student(){
+        cout<<"Default Constructor Called"<<endl;
+    }
+    student(string n, int r){
+        name = n;
+        roll = r;
+    }
+    void display(){
+        cout<<"Name: "<<name<<"\tRollNo: "<<roll<<endl;
+    }
+
+    ~student(){
+        cout<<"Destructor Called\n";
+    }
+};
+
+int main(){
+    student s1,s2("Ali", 32);
+    s2.display();
+return 0;
+}
